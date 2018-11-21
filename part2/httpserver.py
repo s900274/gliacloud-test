6870919540668fe78d80b0e5410a66b1ec50589c
Jeff Wang <s900274@gmail.com>
2018-11-21 12:33:38 +0800

    Add http show zen of python

---
+++ part2/httpserver.py
@@ -0,0 +1,55 @@
+from random import *
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = 8080
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    zenList = [
+        "Beautiful is better than ugly.",
+        "Explicit is better than implicit.",
+        "Simple is better than complex.",
+        "Complex is better than complicated.",
+        "Flat is better than nested.",
+        "Sparse is better than dense.",
+        "Readability counts.",
+        "Special cases aren't special enough to break the rules.",
+        "Although practicality beats purity.",
+        "Errors should never pass silently.",
+        "Unless explicitly silenced.",
+        "In the face of ambiguity, refuse the temptation to guess.",
+        "There should be one-- and preferably only one --obvious way to do it.",
+        "Although that way may not be obvious at first unless you're Dutch.",
+        "Now is better than never.",
+        "Although never is often better than *right* now.",
+        "If the implementation is hard to explain, it's a bad idea.",
+        "If the implementation is easy to explain, it may be a good idea.",
+        "Namespaces are one honking great idea -- let's do more of those!"
+    ]
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        response = self.handle_http(200, self.path)
+        self.wfile.write(response)
+
+    def handle_http(self, status_code, path):
+        self.send_response(status_code)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        content = self.zenList[randint(0, len(self.zenList)-1)]
+        return bytes(content, 'UTF-8')
+
+if __name__ == '__main__':
+    server_class = HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print('Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
